c5a780446f28557600e749531f2810a4cd568236
Sin-14137 <158226032+Sin-14137@users.noreply.github.com>
2024-01-31 17:28:37 -0500

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -13,23 +13,21 @@ public class CardTrick {
         Card[] magicHand = new Card[7]; 
         Random random = new Random();
 
-       
-        Card luckyCard = new Card("Hearts", 12); 
-
         
+        Card luckyCard = new Card("Spades", 1); 
+
+        /
         for (int i = 0; i < magicHand.length; i++) {
             int value = random.nextInt(13) + 1; 
             String suit = Card.SUITS[random.nextInt(Card.SUITS.length)]; 
             magicHand[i] = new Card(suit, value);
         }
 
-        try ( 
-                Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Pick a card! Enter a value (1-13 where 1 is Ace and 13 is King):");
             int userValue = scanner.nextInt();
             System.out.println("Enter a suit (Hearts, Diamonds, Spades, Clubs):");
             String userSuit = scanner.next();
-            
             
             boolean found = false;
             for (Card card : magicHand) {
@@ -39,7 +37,6 @@ public class CardTrick {
                     break;
                 }
             }
-            
             
             if (found) {
                 System.out.println("Your card is in the magic hand or is the lucky card!");
